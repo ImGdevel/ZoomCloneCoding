e9054a73f01fb70b550c150a3aaac59f3a752352
ImGdevel <imdlsrks.mc@gmail.com>
2023-12-28 22:34:14 +0900

    chatting 01

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,14 +1,41 @@
 const socket = new WebSocket(`ws://${window.location.host}`);
+const messageList = document.querySelector('ul');
+const messageForm = document.querySelector('#message');
+const nickForm = document.querySelector('#nick');
+
+function makeMessage(type, message){
+    const msg = {type, message};
+    return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", ()=>{
     console.log("Conncted to Server");
 
 })
 
-socket.addEventListener("message", (message) =>{
-    console.log("just got this:", message.data, "from server");
+socket.addEventListener("message", (m) =>{
+    const message = m.data.toString()
+    const li = document.createElement('li');
+    li.innerText = message;
+    messageList.append(li);
+    
 })
 
 socket.addEventListener("close", (socket)=>{
     console.log("Disonnected server")
 })
+
+
+messageForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("chat", input.value));
+    input.value = "";
+});
+
+nickForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nick", input.value));
+    input.value = "";
+});
--- src/server.js
+++ src/server.js
@@ -15,17 +15,34 @@ app.get("/", (req, res) =>{
 
 const handleListen = () => console.log("Listening on http://localHost:3000")
 
-//console.log("Hello");
-//app.listen(port);
-
 const server = http.createServer(app);
 const wss = new WebSocket.Server({server});
 
+const sockets = [];
 
 wss.on("connection", (socket) => {
     console.log("Connected to Browser");
-    socket.on("close", ()=>{ console.log("Disconnected to Browser");})
-    socket.send("Hello");
+    sockets.push(socket);
+    socket["nickname"] = "Anon"
+
+    socket.on("close", ()=>{ 
+        console.log("Disconnected to Browser");
+    })
+
+    socket.on("message", (message)=>{
+        const parsed = JSON.parse(message.toString());
+        switch(parsed.type){
+            case "chat":
+                sockets.forEach((sock) =>{
+                    sock.send(`${socket.nickname}: ${parsed.message}`)
+                })
+                break;
+            case "nick":
+                socket["nickname"] = parsed.message;
+                break;
+            default:
+        }
+    })
 });
 
 server.listen(3000, handleListen);
